63e69a566d80486072af32a782aaf9431cf09316
Juan Martin Alderete <juanmartinalderetegobbato@gmail.com>
2023-02-06 03:14:15 -0300

    juegos cargados con cards, falta separar categorías

--- js/paginaPrincipal.js
+++ js/paginaPrincipal.js
@@ -2,28 +2,10 @@
 // GLOBALES de Pagina Principal
 
 const juegoDestacadoUI = document.getElementById("juegoDestacado")
-const carruselDeportesUI = document.getElementById("juegoCarrusel")
+const cardsDeportesUI = document.getElementById("cardsDeportes")
 
 
 // Pagina Principal
-
-// Carrusel
-
-let items = document.querySelectorAll('.carousel .carousel-item')
-
-		items.forEach((el) => {
-			const minPerSlide = 4
-			let next = el.nextElementSibling
-			for (var i=1; i<minPerSlide; i++) {
-				if (!next) {
-            // wrap carousel by using first child
-            next = items[0]
-        }
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
-})
 
 
 //  Juego Destacado
@@ -59,23 +41,28 @@ const mostrarDestacado = () =>{
 }
 
 
-const mostrarCarruselDeportes = () => {
+const cargarTarjetasDeportes = () => {
     const juegos = JSON.parse(localStorage.getItem('juegos')) || [];
+        
+    
+    let acumulador = ""
+    for (const juego of juegos){
+        acumulador +=    
+            `<div class="col-12 col-md-3">
+                <div class="card p-1" style="width: 18rem;">
+                <a href="./pages/detalleJuego.html" onclick="redirigirDetalle('${juego.id}')">
+                    <img src="${juego.urlImagen}" class="card-img-top" alt="${juego.titulo}">
+                </a>
+                
+                </div>
+            </div>`
+    }
+    cardsDeportesUI.innerHTML = acumulador;
+
+
+
     
 
-    let acumulador1 = '';
-    for (const juego of juegos) {
-        acumulador1 +=  
-
-        `<div class="col-md-3">
-            <div class="card">
-                <div class="card-img">
-                    <img src="${juego.urlImagen}" class="img-fluid" alt="${juego.titulo}">
-                </div>
-            </div>
-        </div>`
-    }
-    carruselDeportesUI.innerHTML = acumulador1;
 
 
 }
